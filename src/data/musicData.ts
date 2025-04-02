95600c473f923bbaf320446c8b9bab0e412034cc
Shivam <kumarshivam3788@gmail.com>
2025-04-02 22:30:03 +0530

    minor fixes

--- src/data/musicData.ts
+++ src/data/musicData.ts
@@ -6,7 +6,7 @@ const musicData = [
       title: "Starboy",
       artistName: "The Weeknd",
       thumbnail: "https://upload.wikimedia.org/wikipedia/en/3/39/The_Weeknd_-_Starboy.png",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-1.mp3", // Electronic dance music with strong beat
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", // Electronic dance music with strong beat
       duration: "4:16",
       plays: 15000000
     },
@@ -15,7 +15,7 @@ const musicData = [
       title: "Demons",
       artistName: "Imagine Dragons",
       thumbnail: "https://i.scdn.co/image/ab67616d0000b273b2b2747c89d2157b0b29fb6a",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-9.mp3", // Rock-like instrumental
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3", // Rock-like instrumental
       duration: "5:24",
       plays: 12500000
     },
@@ -24,7 +24,7 @@ const musicData = [
       title: "Mouth of the river",
       artistName: "Imagine Dragons",
       thumbnail: "https://images.genius.com/575023edb4b7021498110751743477e5.1000x1000x1.png",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-3.mp3", // Flowing melodic track
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3", // Flowing melodic track
       duration: "6:23",
       plays: 9800000
     },
@@ -33,7 +33,7 @@ const musicData = [
       title: "Ghost Stories",
       artistName: "Coldplay",
       thumbnail: "https://i.scdn.co/image/ab67616d0000b27358d4b554b88532dc00935026",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-8.mp3", // Atmospheric track
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3", // Atmospheric track
       duration: "3:10",
       plays: 8750000
     },
@@ -42,7 +42,7 @@ const musicData = [
       title: "Sparks",
       artistName: "Coldplay",
       thumbnail: "https://m.media-amazon.com/images/I/31lYyvugC5L._UXNaN_FMjpg_QL85_.jpg",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-5.mp3", // Melodic pop
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3", // Melodic pop
       duration: "4:23",
       plays: 7900000
     },
@@ -51,7 +51,7 @@ const musicData = [
       title: "Viva La Vida",
       artistName: "Coldplay",
       thumbnail: "https://i.scdn.co/image/ab67616d0000b273e21cc1db05580b6f2d2a3b6e",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-6.mp3", // Classical influenced
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3", // Classical influenced
       duration: "5:32",
       plays: 18500000
     },
@@ -60,7 +60,7 @@ const musicData = [
       title: "Hymn for the weekend",
       artistName: "Coldplay",
       thumbnail: "https://upload.wikimedia.org/wikipedia/en/3/3d/Coldplay_-_A_Head_Full_of_Dreams.png",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-7.mp3", // Upbeat electronic
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3", // Upbeat electronic
       duration: "2:23",
       plays: 11200000
     },
@@ -69,7 +69,7 @@ const musicData = [
       title: "Pain",
       artistName: "Ryan Jones",
       thumbnail: "https://picsum.photos/id/65/200/200",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-15.mp3", // Dark, moody tones
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-15.mp3", // Dark, moody tones
       duration: "3:12",
       plays: 5800000
     },
@@ -78,7 +78,7 @@ const musicData = [
       title: "Adventure of a Lifetime",
       artistName: "Coldplay",
       thumbnail: "https://upload.wikimedia.org/wikipedia/en/3/3d/Coldplay_-_A_Head_Full_of_Dreams.png",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-2.mp3", // Upbeat, adventure feel
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3", // Upbeat, adventure feel
       duration: "4:23",
       plays: 9200000
     },
@@ -87,7 +87,7 @@ const musicData = [
       title: "The Hills",
       artistName: "The Weeknd",
       thumbnail: "https://i.scdn.co/image/ab67616d0000b2737fcead687e99583072cc217b",
-      musicUrl: "/soundhelix/examples/mp3/SoundHelix-Song-14.mp3", // Dark, haunting feel
+      musicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-14.mp3", // Dark, haunting feel
       duration: "3:41",
       plays: 13700000
     }
